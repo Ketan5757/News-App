11c7c3a3f78fd27860757e2c71fd3e2ece3b5cee
Ketan <ketan.darekar99@gmail.com>
2026-02-04 09:55:44 +0100

    News and NewsItem Component Creation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 
 import React, { Component } from 'react'
+import Navbar from './components/Navbar';
+import News from './components/News';
 
 export default class App extends Component {
-  news = "Latest News"
   render() {
     return (
       <div>
-        Hi and Welcome to News App, here you can view {this.news} 
+        <Navbar/>
+        <News/>
       </div>
     )
   }
---
+++ frontend/src/Components/NewsItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+  render() {
+    return (
+      <div>
+        This cosnist of different news inside main news component
+      </div>
+    )
+  }
+}
+
+export default NewsItem
