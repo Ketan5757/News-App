a663c3db78699f5dba461d88f7635759c4e3d3ac
Ketan <ketan.darekar99@gmail.com>
2026-02-05 09:27:38 +0100

    Card design newsItem Component

--- frontend/src/Components/News.js
+++ frontend/src/Components/News.js
@@ -4,15 +4,41 @@ import NewsItem from './NewsItem'
 export class News extends Component {
   render() {
     return (
-      <div>
-        This is news component
-        <NewsItem title = "News 1" description = "description1"/>
-        <NewsItem title = "News 2" description = "description2"/>
-        <NewsItem title = "News 3" description = "description3"/>
-        <NewsItem title = "News 4" description = "description4"/>
-        <NewsItem title = "News 4" description = "description4"/>
-        <NewsItem title = "News 5" description = "description5"/>
+      <>    
+        <div className="container my-4">
+          <h1>AlleNews - An Entry Point to view latest News</h1>
+          <div className="row">
+            <div className="col-md-4">
+            <NewsItem title = "News 1" description = "description1"/>
+            </div>
+            <div className="col-md-4">
+            <NewsItem title = "News 2" description = "description2"/>
+            </div>
+            <div className="col-md-4">
+            <NewsItem title = "News 3" description = "description3"/>
+            </div>
+            <div className="col-md-4 my-4">
+            <NewsItem title = "News 4" description = "description4"/>
+            </div>
+            <div className="col-md-4 my-4">
+            <NewsItem title = "News 5" description = "description5"/>
+            </div>
+            <div className="col-md-4 my-4">
+            <NewsItem title = "News 6" description = "description6"/>
+            </div>
+            <div className="col-md-4 my-4">
+            <NewsItem title = "News 7" description = "description7"/>
+            </div>
+            <div className="col-md-4 my-4">
+            <NewsItem title = "News 8" description = "description8"/>
+            </div>
+            <div className="col-md-4 my-4">
+            <NewsItem title = "News 9" description = "description9"/>
+            </div>
+
+          </div>
       </div>
+      </>
     )
   }
 }
--- frontend/src/Components/NewsItem.js
+++ frontend/src/Components/NewsItem.js
@@ -6,7 +6,7 @@ export class NewsItem extends Component {
     return (
       <div>
         <div className="card" style= {{width: "18rem"}}>
-        <img src="..." className="card-img-top" alt="..."/>
+        <img src="https://images.bild.de/6982ea58e2529c3c38c298ca/3c72057c7a3b05b9c6b2fb619d2736f0,cc1f8317?w=1280" className="card-img-top" alt="..."/>
         <div className="card-body">
         <h5 className="card-title">{title}</h5>
          <p className="card-text">{description}</p>
