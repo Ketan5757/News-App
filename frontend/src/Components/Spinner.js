16c1197250490c0f49bb599af5866861b33bc1cb
Ketan <ketan.darekar99@gmail.com>
2026-02-12 09:24:14 +0100

    Minor fix

--- frontend/src/Components/Spinner.js
+++ frontend/src/Components/Spinner.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react'
-import spinner from '../Assets/spinner.gif'
-export default class Spinner extends Component {
-  render() {
-    return (
-      <div>
-        <div className="text-center">
-            <img src={spinner} alt="loading" />
-        </div>
-      </div>
-    )
-  }
-}
+// import React, { Component } from 'react'
+// import spinner from '../Assets/spinner.gif'
+// export default class Spinner extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <div className="text-center">
+//             <img src={spinner} alt="loading" />
+//         </div>
+//       </div>
+//     )
+//   }
+// }
 
